439a95a94b0dd1a1957307c62c0c6dc34d4511ba
Louis Lin <linlo@oregonstate.edu>
2022-07-20 02:24:59 +0800

    cs 361 microservice

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const spell = require('spell-checker-js');
+spell.load('en');
+
+app.use(express.json());
+app.enable('trust proxy');
+
+app.get('/', (req, res) => {
+  res.status(200).send('works!');
+});
+
+app.post('/', (req, res) => {
+  const check = spell.check(req.body.text);
+  const checkRemoveNum = check.filter((word) => isNaN(parseFloat(word)));
+  res.status(200).json({ invalidWords: checkRemoveNum });
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
